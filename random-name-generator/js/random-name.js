a576052e65659dbe65a205c69078bff454db847c
manu-hn <manuu.hn11@gmail.com>
2023-11-09 17:36:32 +0530

    Made Some HTML template

---
+++ random-name-generator/js/random-name.js
@@ -0,0 +1,39 @@
+export const firstName = {
+    0: "Code",
+    1: "Learning",
+    2: "Coding", 
+    3: "Program", 
+    4: "Coder",
+    5: "Programming", 
+    6: "Dev", 
+    7: "WebDev", 
+    8: "Web", 
+    9: "Tech",
+    10: "Ninja",
+    11: "Cyber", 
+    12: "JavaScript", 
+    13: "Python",
+    14: "Intelligent",
+    15: "Digital"
+}
+
+export const lastName = {
+    0: "Life", 
+    1: "MadeEasy", 
+    2: "Programmer", 
+    3: "Simplified", 
+    4: "Development", 
+    5: "World",
+    6: "Media", 
+    7: "Developer", 
+    8: "Stacker", 
+    9: "WebDev",
+    10: "Balance",
+    11: "Master",
+    12: "Pioneer",
+    13: "Prodigy",
+    14: "Genius",
+    15: "Guru",
+    16: "Engineer",
+    17: "Sorcerer"
+}
---
+++ random-name-generator/js/vanilla.js
@@ -0,0 +1,8 @@
+import {firstName, lastName} from "./random-name";
+
+const initialize= function initFunction(){
+    document.getElementById('name-form').addEventListener('submit', (event)=>{
+        event.preventDefault();
+        
+    })
+}
